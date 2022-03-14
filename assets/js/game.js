7756ed9976372879586091a3e914b7799d58c079
Vani Karun <vanikarun21@yahoo.com>
2022-03-13 21:31:29 -0500

    updated robots with if and else commands to view healtcheck,alive, dead status

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,6 +22,14 @@ console.log(
   playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
 );
 
+// check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+
 // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
 playerHealth = playerHealth - enemyAttack;
 
@@ -29,6 +37,14 @@ playerHealth = playerHealth - enemyAttack;
 console.log(
   enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
 );
+
+// check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
  
 };
 // execute function
