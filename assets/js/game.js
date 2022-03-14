32711ae159b010b07e4ef362d65a6182529e052e
Vani Karun <vanikarun21@yahoo.com>
2022-03-14 14:33:09 -0500

    additional robots added, for, arrays and loop commands added and executed

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,26 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth, playerMoney);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+
+console.log(enemyNames);
+
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
+
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyName, enemyAttack,enemyHealth);
+
+
+console.log(enemyNames, enemyAttack,enemyHealth);
 
 var fight = function() {
   window.alert("Welcome to Robot Gladiators!");
@@ -64,4 +79,4 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
  
 };
 // execute function
-fight();
+//fight();
